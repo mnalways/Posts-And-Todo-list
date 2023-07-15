16463d1ec9c90b51fa23e9e5ea6c54cae8f0353f
Mayank Nigam <mayank.nigam@thoughtspot.com>
2023-07-15 10:29:59 +0530

    Mutate todos error handling and enrich user exp on loading

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -7,7 +7,7 @@ const TodoForm = () => {
   const endPoint = "/todos";
   const ref = useRef<HTMLInputElement>(null);
   const queryClient = useQueryClient();
-  const updateTodo = useMutation({
+  const updateTodo = useMutation<Todo, Error, Todo>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>(`https://jsonplaceholder.typicode.com${endPoint}`, todo)
@@ -21,31 +21,39 @@ const TodoForm = () => {
         UpdatedTodo,
         ...(todos || []),
       ]);
+      if (ref.current) ref.current.value = "";
     },
   });
 
   return (
-    <form
-      className="row mb-3"
-      onSubmit={(event) => {
-        event.preventDefault();
-        //mutate
-        if (ref.current && ref.current.value)
-          updateTodo.mutate({
-            id: 0,
-            title: ref.current?.value,
-            userId: 1,
-            completed: true,
-          });
-      }}
-    >
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
+    <>
+      {updateTodo.error ? (
+        <div className="alert alert-danger">{updateTodo.error?.message}</div>
+      ) : null}
+      <form
+        className="row mb-3"
+        onSubmit={(event) => {
+          event.preventDefault();
+          //mutate
+          if (ref.current && ref.current.value)
+            updateTodo.mutate({
+              id: 0,
+              title: ref.current?.value,
+              userId: 1,
+              completed: true,
+            });
+        }}
+      >
+        <div className="col">
+          <input ref={ref} type="text" className="form-control" />
+        </div>
+        <div className="col">
+          <button disabled={updateTodo.isLoading} className="btn btn-primary">
+            {updateTodo.isLoading ? "...Adding" : "Add"}
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
