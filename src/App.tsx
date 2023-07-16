5c6f79f506fdf420098c6b41e040bbc18839466e
Mayank Nigam <mayank.nigam@thoughtspot.com>
2023-07-16 21:43:34 +0530

    Counter useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import "./App.css";
 import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
+import Counter from "./state-management/Counter";
 
 function App() {
   return (
     <>
-      <TodoForm />
-      <TodoList />
+      {/* <TodoForm />
+      <TodoList /> */}
       {/* <h1>Posts</h1>
       <PostList /> */}
+      <Counter />
     </>
   );
 }
