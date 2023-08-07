ea84925698b89168519ced9b31a10d414743e3d7
Mayank Nigam <mayank.nigam@thoughtspot.com>
2023-08-07 23:04:38 +0530

    nested navigation NavBar

--- src/routing/Router.tsx
+++ src/routing/Router.tsx
@@ -1,14 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
-import ContactPage from "./ContactPage";
 import UserDetailPage from "./UserDetailPage";
+import Layout from "./Layout";
 
 const Router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/users", element: <UserListPage /> },
-  //   { path: "/contact", element: <ContactPage /> },
-  { path: "/users/:id", element: <UserDetailPage /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "users", element: <UserListPage /> },
+      { path: "users/:id", element: <UserDetailPage /> },
+    ],
+  },
 ]);
 
 export default Router;
