d0c539e691a9087ead8710df58525302d30013af
Mayank Nigam <mayank.nigam@thoughtspot.com>
2023-08-07 22:11:21 +0530

    useParams, useSearchParams,

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,13 @@
+import { useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  console.log("params", params);
+  // For future Revision
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // console.log('searchParams.get("name")', searchParams.get("name"));
+  // console.log("searchParams.toString()", searchParams.toString());
+
   return <p>User</p>;
 };
 
