7a99495c7656446295cf8fcb8472bf50de332a5b
Mayank Nigam <mayank.nigam@thoughtspot.com>
2023-08-07 10:57:16 +0530

    part-4 React Router router file, multi page, link, useNavigate

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,15 @@ import App from "./App";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "react-router-dom";
+import Router from "./routing/Router";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={Router} />
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
--- src/routing/ContactPage.tsx
+++ src/routing/ContactPage.tsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 const ContactPage = () => {
+  const navigation = useNavigate();
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
+        navigation("/");
         // Redirect the user to the home page
       }}
     >
