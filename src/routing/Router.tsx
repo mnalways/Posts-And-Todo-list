71560439361afd9ac53e3ff76264139682b246f7
Mayank Nigam <mayank.nigam@thoughtspot.com>
2023-08-07 22:05:00 +0530

    Routing link and params

--- src/routing/Router.tsx
+++ src/routing/Router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
 import ContactPage from "./ContactPage";
+import UserDetailPage from "./UserDetailPage";
 
 const Router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/users", element: <UserListPage /> },
-  { path: "/contact", element: <ContactPage /> },
+  //   { path: "/contact", element: <ContactPage /> },
+  { path: "/users/:id", element: <UserDetailPage /> },
 ]);
 
 export default Router;
