51755b8a2345b2763e08a8d665c6191b92340ffe
Mayank Nigam <mayank.nigam@thoughtspot.com>
2023-08-10 18:38:38 +0530

    Nav and routing error

--- src/routing/Router.tsx
+++ src/routing/Router.tsx
@@ -3,11 +3,13 @@ import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
 import UserDetailPage from "./UserDetailPage";
 import Layout from "./Layout";
+import ErrorPage from "./ErrorPage";
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
